637be6ee9f9b127c9a31606613098d28a0440bab
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-06-24 14:54:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const p = document.getElementById("status");
+const b = document.getElementById("enterBtn");
+function func() {
+	p.innerText = "";
+	var h1 = document.createElement("h1");
+	h1.innerText = "Entered Metaverse";
+	p.appendChild(h1);
+}
+b.addEventListener("click",func);
+
